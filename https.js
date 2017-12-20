a56d60d15c9815c9288584569e1d31e658eadc09
pvthanh38 <pvthanh38@gmail.com>
2017-12-20 14:52:16 +0700

    7

--- https.js
+++ https.js
@@ -1,11 +1,18 @@
 var https = require('https')
 var pem = require('pem')
+var express = require('express')
 
 pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
   if (err) {
     throw err
   }
-  https.createServer({ key: keys.serviceKey, cert: keys.certificate }, function (req, res) {
-    res.end('o hai!')
-  }).listen(3000)
+  var app = express()
+
+  app.get('/', function (req, res) {
+	console.log(req.param('hub.challenge'));
+	res.send(req.param('hub.challenge'))
+    res.send('o hai!')
+  })
+
+  https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(443)
 })
