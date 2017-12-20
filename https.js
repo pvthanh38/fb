db3af578bbf23ff9cf41e3dde46d205aef6b1291
pvthanh38 <pvthanh38@gmail.com>
2017-12-20 16:05:43 +0700

    9

--- https.js
+++ https.js
@@ -13,6 +13,33 @@ pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
 	res.send(req.param('hub.challenge'))
     res.send('o hai!')
   })
+  
+  app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "123456"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+});
 
   https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(3000)
 })
