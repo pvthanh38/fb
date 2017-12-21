72cbc0e258bc7a8003d8435b85a7ac54213801c9
pvthanh38 <pvthanh38@gmail.com>
2017-12-21 10:17:16 +0700

    10

--- ssl.js
+++ ssl.js
@@ -5,8 +5,8 @@ const express = require('express');
 const app = express();
 // Set up express server here
 const options = {
-    cert: fs.readFileSync('./sslcert/fullchain.pem'),
-    key: fs.readFileSync('./sslcert/privkey.pem')
+    cert: fs.readFileSync('/etc/letsencrypt/live/lab.letweb.net/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/lab.letweb.net/privkey.pem')
 };
 app.get('/', function (req, res) {
 	res.send('Hello world, I am a chat bot')
